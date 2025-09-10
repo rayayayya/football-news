9c3303f6c2efd77d5bd9c4693d70f4b9bff58dde
rayayayya <raynabalq.s@gmail.com>
2025-09-10 14:09:36 +0700

    cek

--- main/views.py
+++ main/views.py
@@ -37,15 +37,9 @@ def show_news(request, id):
     return render(request, "news_detail.html", context)
 
 def show_xml(request):
-    news_list = News.objects.all()
-
-def show_xml(request):
      news_list = News.objects.all()
      xml_data = serializers.serialize("xml", news_list)
      return HttpResponse(xml_data, content_type="application/xml")
-
-def show_json(request):
-    news_list = News.objects.all()
 
 def show_json(request):
     news_list = News.objects.all()
